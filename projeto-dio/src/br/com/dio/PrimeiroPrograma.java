a9095a927008e31cd561e4a0c99337cd9e7efdf3
JoseLuisCorreia <82620439+JoseLuisCorreia@users.noreply.github.com>
2021-09-14 15:34:43 -0300

    first committ

--- projeto-dio/src/br/com/dio/PrimeiroPrograma.java
+++ projeto-dio/src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,48 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro = new Livro("Acertei", 300);
+
+
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello world! " + (a+b));*/
+    }
+
+}
+class Livro{
+    String nome;
+    Integer numPaginas;
+    public Livro() {
+    }
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
